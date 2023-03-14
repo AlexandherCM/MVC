13b43d7cc1e427f68b6502a7d90d87fc501ee02b
AlexandherCM <alexandher_1321124129@uptecamac.edu.mx>
2023-03-13 20:11:47 -0600

    MVC

---
+++ MVC/MVC/Controllers/PeliculasController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class PeliculasController : Controller
+    {
+        private readonly MVCContext _context;
+
+        public PeliculasController(MVCContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Peliculas
+        public async Task<IActionResult> Index()
+        {
+              return _context.Pelicula != null ? 
+                          View(await _context.Pelicula.ToListAsync()) :
+                          Problem("Entity set 'MVCContext.Pelicula'  is null.");
+        }
+
+        // GET: Peliculas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Pelicula == null)
+            {
+                return NotFound();
+            }
+
+            var pelicula = await _context.Pelicula
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Peliculas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Titulo,Lanzamiento,Genero,Precio")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pelicula);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Pelicula == null)
+            {
+                return NotFound();
+            }
+
+            var pelicula = await _context.Pelicula.FindAsync(id);
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Titulo,Lanzamiento,Genero,Precio")] Pelicula pelicula)
+        {
+            if (id != pelicula.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pelicula);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PeliculaExists(pelicula.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Pelicula == null)
+            {
+                return NotFound();
+            }
+
+            var pelicula = await _context.Pelicula
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Pelicula == null)
+            {
+                return Problem("Entity set 'MVCContext.Pelicula'  is null.");
+            }
+            var pelicula = await _context.Pelicula.FindAsync(id);
+            if (pelicula != null)
+            {
+                _context.Pelicula.Remove(pelicula);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PeliculaExists(int id)
+        {
+          return (_context.Pelicula?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ MVC/MVC/Models/Pelicula.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC.Models
+{
+    [Table("PELICULA")]
+    public class Pelicula
+    {
+        [Key] 
+        public int ID { get; set; }
+
+        [Required]
+        public string? Titulo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Lanzamiento { get; set; }
+        public string? Genero { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
